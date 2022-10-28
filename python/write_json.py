53d9223e70e05376df3291e83a7d91d6e73256a5
Josh Powers <powersj@fastmail.com>
2022-10-28 12:12:53 -0600

    python: add json parsing example

---
+++ python/write_json.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Example to read JSON data and send to InfluxDB."""
+
+from dateutil import parser
+import json
+
+from influxdb_client import InfluxDBClient, Point, WriteOptions
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+point = Point("weather")
+with open("../data/weather.json", "r") as json_file:
+    data = json.load(json_file)
+
+    point.tag("station", data["name"])
+    point.time(parser.parse(data["updated"]))
+    for sensor in data["sensors"]:
+        point.field(sensor["name"], sensor["value"])
+
+with InfluxDBClient.from_config_file("config.toml") as client:
+    with client.write_api(write_options=SYNCHRONOUS) as writer:
+        try:
+            writer.write(bucket="my-bucket", record=[point])
+        except InfluxDBError as e:
+            print(e)
