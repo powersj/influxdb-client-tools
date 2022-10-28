3bc2d58f5e1ed4bf6903aab53c7f0faa9261e470
Josh Powers <powersj@fastmail.com>
2022-10-28 12:12:38 -0600

    python: update fields for data, black all code

--- python/write_csv.py
+++ python/write_csv.py
@@ -1,5 +1,10 @@
+#!/usr/bin/env python3
+"""Example to read CSV data and send to InfluxDB."""
+
 import pandas as pd
 from influxdb_client import InfluxDBClient, WriteOptions
+from influxdb_client.client.exceptions import InfluxDBError
+
 
 with InfluxDBClient.from_env_properties() as client:
     for df in pd.read_csv("../data/vix.csv", chunksize=10_000):
@@ -10,7 +15,7 @@ with InfluxDBClient.from_env_properties() as client:
                     bucket="my-bucket",
                     data_frame_measurement_name="stocks",
                     data_frame_tag_columns=["symbol"],
-                    data_frame_timestamp_column="date",
+                    data_frame_timestamp_column="timestamp",
                 )
             except InfluxDBError as e:
                 print(e)
